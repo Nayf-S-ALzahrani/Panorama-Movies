40d10996aeed9acb54dafab10320b94ec518547b
Nayef Saeed Alzahrani <NAYEF.SAEED.jed.k2@tuwaiq.edu.sa>
2022-02-14 20:57:49 +0300

    - Delete Web View View model

--- app/src/main/java/com/example/myproject/presentation/webview_list/WebViewValue.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myproject.presentation.webview_list
-
-import com.example.myproject.domain.model.detail.ShowDetail
-
-data class WebViewValue (
-val isLoading: Boolean = false,
-val trailer: ShowDetail? = null ,
-val error: String = "",
-)
